c413d6c83e48bcd2186ae993c8e8d1a508faaf34
Kingjiyun <etock19@naver.com>
2023-05-30 19:13:29 +0900

    직접풀어보기 5-2

---
+++ app/src/main/java/com/example/layouttest2j/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.layouttest2j
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dir5_2)
+    }
+}
